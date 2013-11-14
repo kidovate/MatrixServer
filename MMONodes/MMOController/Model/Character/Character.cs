7199a682209e69a5a49b0cdc84e3931e18e7e482
Christian Stewart <kidovate@gmail.com>
2013-11-14 14:48:02 -0800

    Add character model

---
+++ MMONodes/MMOController/Model/Character/Character.cs
@@ -0,0 +1,36 @@
+using System;
+using MMOCommon;
+
+namespace MMOController.Model.Character
+{
+	/// <summary>
+	/// The main database model for a character in the system.
+	/// </summary>
+	public class Character 
+	{
+		/// <summary>
+		/// The ID of the character.
+		/// </summary>
+		/// <value>The identifier.</value>
+		public virtual int Id { get; protected set; }
+
+		/// <summary>
+		/// Human Readable name of the character.
+		/// </summary>
+		/// <value>The full name.</value>
+		public virtual string Name {get;set;}
+
+		/// <summary>
+		/// Gender of the character. True is male.
+		/// </summary>
+		/// <value>Gender.</value>
+		public virtual bool Gender {get;set;}
+
+		/// <summary>
+		/// XP of the character.
+		/// </summary>
+		/// <value>Character XP.</value>
+		public virtual int XP {get;set;}
+	}
+}
+
---
+++ MMONodes/MMOController/Model/Character/CharacterMap.cs
@@ -0,0 +1,21 @@
+using System;
+using FluentNHibernate.Mapping;
+
+namespace MMOController.Model.Character
+{
+	/// <summary>
+	/// Mapping the <see cref="Character"/> to the database. 
+	/// </summary>
+	public class CharacterMap : ClassMap<Character>
+	{
+		public CharacterMap ()
+		{
+			Id(x=>x.Id);
+		 	Map(x=>x.Name)
+        .Unique();
+		  Map(x => x.Gender);
+			Map(x=>x.XP);
+		}
+	}
+}
+
